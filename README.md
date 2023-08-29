04ecb1746a4db0a38a339d0288706f2d31cd56c5
wtrinh02 <yelsew02@gmail.com>
2023-08-28 17:48:37 -0700

    Finished project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# JsSiteTemplate
-Site Template for JS Projects
+# RaceCar
+Race Car is a palindrome. A palindrome is a word, phrase, or sequence that reads the same backward as forward. Write a program that checks if the phrase entered is a palindrome.
--- js/site.js
+++ js/site.js
@@ -1,4 +1,42 @@
-//Call hello World
-function HelloWorld(){
-    alert("Hello World");
+
+function getValue(){
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let returnObj = isPalindrome(userString);
+
+    displayMessage(returnObj);
+}
+
+function isPalindrome(userString){
+    userString = userString.toLowerCase();
+
+    let regex = /[^a-z0-9]/gi;
+    userString = userString.replace(regex,"");
+
+    let revString =[];
+    let returnObj = {};
+
+    for(let i = userString.length - 1; i>=0; i--){
+        revString += userString[i];
+    }
+
+    if (revString == userString){
+        returnObj.msg = "Well done! You entered a palindrome!"
+    }
+    else{
+        returnObj.msg = "Sorry! You did not enter a palindrome!"
+    }
+
+    returnObj.reversed = revString;
+
+    return returnObj;
+}
+
+function displayMessage(returnObj){
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML=`Your phrase reversed is: ${returnObj.reversed}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
